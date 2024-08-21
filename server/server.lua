cf7b0dde86e117621a7771f0fe8749462c91a0b3
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-08-21 06:27:19 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,36 +1,5 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
------------------------------------------------------------------------
--- version checker
------------------------------------------------------------------------
-local function versionCheckPrint(_type, log)
-    local color = _type == 'success' and '^2' or '^1'
-
-    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
-end
-
-local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-telegram/main/version.txt', function(err, text, headers)
-        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
-
-        if not text then
-            versionCheckPrint('error', 'Currently unable to run a version check.')
-            return
-        end
-
-        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
-        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
-
-        if text == currentVersion then
-            versionCheckPrint('success', 'You are running the latest version.')
-        else
-            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
-        end
-    end)
-end
-
------------------------------------------------------------------------
-
 -- Make Bird Post as a Usable Item
 RSGCore.Functions.CreateUseableItem("birdpost", function(source)
     TriggerClientEvent("rsg-telegram:client:WriteMessage", source)
@@ -244,8 +213,3 @@ RSGCore.Commands.Add('addressbook', 'Your Personal Addressbook', {}, false, func
     local src = source
     TriggerClientEvent('rsg-telegram:client:OpenAddressbook', src)
 end)
-
---------------------------------------------------------------------------------------------------
--- start version check
---------------------------------------------------------------------------------------------------
-CheckVersion()
